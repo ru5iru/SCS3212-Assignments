26a024a58dc52e307ee5236545364101d938f49f
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-07-14 01:05:46 +0530

    Add files via upload

---
+++ Practical 05/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.simplecalculator
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textResult: TextView
+    private var operand1: Double = 0.0
+    private var operand2: Double = 0.0
+    private var operator: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textResult = findViewById(R.id.textResult)
+
+        val button0 = findViewById<Button>(R.id.button0)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        val buttonSubtract = findViewById<Button>(R.id.buttonSubtract)
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        val buttonClear = findViewById<Button>(R.id.buttonClear)
+
+        button0.setOnClickListener { appendNumber("0") }
+        button1.setOnClickListener { appendNumber("1") }
+        button2.setOnClickListener { appendNumber("2") }
+        button3.setOnClickListener { appendNumber("3") }
+        button4.setOnClickListener { appendNumber("4") }
+        button5.setOnClickListener { appendNumber("5") }
+        button6.setOnClickListener { appendNumber("6") }
+        button7.setOnClickListener { appendNumber("7") }
+        button8.setOnClickListener { appendNumber("8") }
+        button9.setOnClickListener { appendNumber("9") }
+
+        buttonAdd.setOnClickListener { setOperator("+") }
+        buttonSubtract.setOnClickListener { setOperator("-") }
+        buttonMultiply.setOnClickListener { setOperator("*") }
+        buttonDivide.setOnClickListener { setOperator("/") }
+
+        buttonEquals.setOnClickListener { calculateResult() }
+        buttonClear.setOnClickListener { clear() }
+    }
+
+    private fun appendNumber(number: String) {
+        textResult.append(number)
+    }
+
+    private fun setOperator(op: String) {
+        operand1 = textResult.text.toString().toDouble()
+        operator = op
+        textResult.text = ""
+    }
+
+    private fun calculateResult() {
+        operand2 = textResult.text.toString().toDouble()
+        var result = 0.0
+
+        when (operator) {
+            "+" -> result = operand1 + operand2
+            "-" -> result = operand1 - operand2
+            "*" -> result = operand1 * operand2
+            "/" -> result = operand1 / operand2
+        }
+
+        textResult.text = result.toString()
+    }
+
+    private fun clear() {
+        textResult.text = ""
+        operand1 = 0.0
+        operand2 = 0.0
+        operator = ""
+    }
+}
---
+++ Practical 05/README.md
@@ -0,0 +1,19 @@
+# Simple-Calculator-Kotlin
+
+1. Create a new Android project in Android Studio.
+2. Design the layout for the calculator interface. Use a LinearLayout or
+RelativeLayout as the root view group, and add the following views
+TextView: Display the input and output of the calculator.
+Buttons: Create buttons for numbers (0-9), addition (+), subtraction
+(-), multiplication (*), division (/), and equals (=).
+3. Implement the event handling logic for the buttons
+Add onClick listeners to the number buttons to update the input text view.
+Add onClick listeners to the operator buttons (+, -, *, /) to store the selected
+operator.
+4. Add onClick listener to the equals button to perform the calculation and
+display the result.
+5. Implement the calculation logic
+Create a function to perform the actual calculation based on the selected
+operator and display the result in the text view.
+Handle error cases such as dividing by zero.
+6. Test the application by running it on an emulator or device.
